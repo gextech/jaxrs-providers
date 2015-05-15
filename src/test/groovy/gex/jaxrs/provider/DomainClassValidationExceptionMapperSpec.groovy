ba2891565ecc1536091c6a6b400da83d0b5730ea
Domingo Suarez Torres <domingo.suarez@gmail.com>
2015-05-14 23:50:26 -0500

    wip for 0.1.0 release

--- src/test/groovy/gex/jaxrs/provider/DomainClassValidationExceptionMapperSpec.groovy
+++ src/test/groovy/gex/jaxrs/provider/DomainClassValidationExceptionMapperSpec.groovy
@@ -16,7 +16,6 @@
  */
 package gex.jaxrs.provider
 
-import gex.commons.exception.DomainClassValidationException
 import gex.jaxrs.ApiResponse
 import org.springframework.context.ApplicationContext
 import org.springframework.validation.Errors
@@ -32,67 +31,7 @@ import static java.util.Collections.emptyMap
 /**
  * Created by domix on 12/23/14.
  */
-class DomainClassValidationExceptionMapperSpec extends Specification {
-  @Shared
-  Map entity
-  @Shared
-  DomainClassValidationExceptionMapper exceptionMapper
-  @Shared
-  Errors errors
 
-  def setup() {
-    entity = [
-      message         : "The provided entity has some errors.",
-      validationErrors: emptyList(),
-      code            : "",
-      extraData       : emptyMap(),
-      i18nCode        : ""
-    ]
-
-    errors = Spy(Errors)
-    errors.getFieldErrors() >> emptyList()
-    errors.getFieldErrors()
-
-    def mockResponse = Mock(Response)
-    mockResponse.getEntity() >> entity
-    mockResponse.getStatus() >> 422
-
-    ApiResponse apiResponse = Spy(ApiResponse)
-    apiResponse.buildResponse(_, _) >> mockResponse
-
-    ApplicationContext applicationContext = Mock(ApplicationContext)
-
-    exceptionMapper = new DomainClassValidationExceptionMapper(apiResponse: apiResponse, applicationContext: applicationContext)
-  }
-
-  @Unroll
-  def "should create a Response with default values for '#label'"() {
-    given:
-      def response = exceptionMapper.toResponse(new DomainClassValidationException(object))
-    expect:
-      response
-      response.entity == entity
-      response.status == 422
-    where:
-      object                       | label
-      new Object()                 | 'generic Object'
-      new ObjectWithErrorsField()  | 'object with field'
-      new ObjectWithErrorsMethod() | 'object with method'
-
-  }
-
-  def 'should create a Response when the object has errors field'() {
-    setup:
-      def object = new ObjectWithErrorsField()
-      object.errors = errors
-    when:
-      def response = exceptionMapper.toResponse(new DomainClassValidationException(object))
-    then:
-      response
-  }
-
-
-}
 
 class ObjectWithErrorsField {
   Errors errors
